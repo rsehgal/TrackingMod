b0049c23a1bd6a655224661e7a84583f6c7a2880
Raman Sehgal <raman.sehgal@cern.ch>
2016-06-03 08:23:24 +0530

    Added marker at the point of intersection with plane. The code works fine for 6 detector, but if number of detector become 8 , then the tracks are not generated as expected.

--- TemplatizedInterface/inc/Update.h
+++ TemplatizedInterface/inc/Update.h
@@ -20,128 +20,114 @@
 #include "TEveStraightLineSet.h"
 #include "TRandom.h"
 
-namespace Tomography{
+namespace Tomography {
 
-
-class Update{
+class Update {
 
 public:
-	Update(){}
-	void *handle(void *ptr) {
+  Update() {}
+  void *handle(void *ptr) {
 
-	          TGeoHMatrix m;
-	          LinesAngle l;
-	          Double_t trans[3] = {0., 0., 0.};
-	          m.SetTranslation(trans);
-	          m.SetDx(rand()%50);
-	          m.SetDy(rand()%50);
-	          m.SetDz(75);
+    TGeoHMatrix m;
+    LinesAngle l;
+    Double_t trans[3] = {0., 0., 0.};
+    m.SetTranslation(trans);
+    m.SetDx(rand() % 50);
+    m.SetDy(rand() % 50);
+    m.SetDz(75);
 
-	          Coordinates c;
-	          Tracking::Vector3D<double> temp;
-	          //TEveStraightLineSet *ls = new TEveStraightLineSet();
-	          std::vector<HittedPixel*> hittedPixelVector;
-	          while(true){
-	            //sleep(2);
+    Coordinates c;
+    Tracking::Vector3D<double> temp;
+    // TEveStraightLineSet *ls = new TEveStraightLineSet();
+    std::vector<HittedPixel *> hittedPixelVector;
+    while (true) {
+      // sleep(2);
+      // c.CoGenerator(Tomography::SetupManager::instance()->GetDetectorVector("GLASS"));
+      c.CoGenerator(0);
+      c.SetStrips();
+      c.SetStripCoordinates();
 
-	            //c.CoGenerator(Tomography::SetupManager::instance()->GetDetectorVector("GLASS"));
-	            c.CoGenerator(0);
-	            c.SetStrips();
-	            c.SetStripCoordinates();
+      Vector3D<double> temp2 = c.GetSpecificCoordinate(10.);
+      Coordinates c2(temp2);
+      c2.CoGenerator(1);
+      c2.SetStrips();
+      c2.SetStripCoordinates();
 
-	            Vector3D<double>temp2 = c.GetSpecificCoordinate(10.);
-	            Coordinates c2(temp2);
-	            c2.CoGenerator(1);
-	            c2.SetStrips();
-	            c2.SetStripCoordinates();
+      // count++;
+      TGeoBBox *shape;
+      std::cout << "Size : " << hittedPixelVector.size() << std::endl;
+      if (hittedPixelVector.size()) {
+        for (int i = 0; i < hittedPixelVector.size(); i++) {
+          Tracking::Singleton::instance()->RemoveElement(hittedPixelVector[i]->GetEveGeoShape());
+        }
+      }
 
-	            //count++;
-	            TGeoBBox *shape ;
-	            std::cout<<"Size : "<< hittedPixelVector.size() << std::endl;
-	            if(hittedPixelVector.size()){
-	              for(int i = 0 ; i < hittedPixelVector.size() ; i++){
-	                Tracking::Singleton::instance()->RemoveElement(hittedPixelVector[i]->GetEveGeoShape());
-	              }
-	              //Tracking::Singleton::instance()->RemoveElement();
-	            }
+      hittedPixelVector.clear();
+      //For top half detectors
+      for (int detNo = 0; detNo < Tomography::SetupManager::instance()->GetLowerLayerStartIndex(); detNo++) {
+        temp = c.GetStripCoordinate(detNo + 1);
+        temp.Print();
+        m.SetDx(temp.x());
+        m.SetDy(temp.y());
+        m.SetDz(temp.z());
+        // Add some element
+        if (gEve) {
+          hittedPixelVector.push_back(new HittedPixel(m));
+          Tracking::Singleton::instance()->AddElement(hittedPixelVector[detNo]->GetEveGeoShape());
+        }
+      }
 
-	            //if( Tracking::Singleton::instance()->GetLineSet())
-	            //  Tracking::Singleton::instance()->RemoveElement(ls); //Tracking::Singleton::instance()->GetLineSet());
+      //For bottom half detectors
+      for (int detNo = Tomography::SetupManager::instance()->GetLowerLayerStartIndex();
+           detNo < Tomography::SetupManager::instance()->GetDetectorVector("GLASS").size(); detNo++) {
+        // for(int detNo = 0 ; detNo < Tomography::SetupManager::instance()->GetLowerLayerStartIndex() ; detNo++){
+        temp = c2.GetStripCoordinate(detNo - Tomography::SetupManager::instance()->GetLowerLayerStartIndex() + 1);
+        temp.Print();
+        m.SetDx(temp.x());
+        m.SetDy(temp.y());
+        m.SetDz(temp.z());
 
+        // Add some element
+        if (gEve) {
+          hittedPixelVector.push_back(new HittedPixel(m));
+          Tracking::Singleton::instance()->AddElement(hittedPixelVector[detNo]->GetEveGeoShape());
+        }
+      }
 
-	            hittedPixelVector.clear();
-	            for(int detNo = 0 ; detNo < Tomography::SetupManager::instance()->GetLowerLayerStartIndex() ; detNo++){
-	              temp = c.GetStripCoordinate(detNo+1);
-	              temp.Print();
-	              m.SetDx(temp.x());
-	              m.SetDy(temp.y());
-	              m.SetDz(temp.z());
+      std::cout << "Pt : ";
+      c.GetCoordinate(1).Print();
+      TEveStraightLineSet *ls = new TEveStraightLineSet();
+      ls->AddLine(c.GetCoordinate(1).x(), c.GetCoordinate(1).y(), c.GetCoordinate(1).z(), c.GetCoordinate(3).x(),
+                  c.GetCoordinate(3).y(), c.GetCoordinate(3).z());
 
-	            //m.SetDx(*temp);
-				//m.SetDy(*(temp + 1));
-				//m.SetDz(*(temp + 2));
+      ls->AddMarker(c.GetCoordinate(1).x(), c.GetCoordinate(1).y(), c.GetCoordinate(1).z());
+      ls->AddMarker(c.GetCoordinate(2).x(), c.GetCoordinate(2).y(), c.GetCoordinate(2).z());
+      ls->AddMarker(c.GetCoordinate(3).x(), c.GetCoordinate(3).y(), c.GetCoordinate(3).z());
 
-	            //Add some element
-	            if(gEve){
-	              hittedPixelVector.push_back(new HittedPixel(m));
-	              Tracking::Singleton::instance()->AddElement(hittedPixelVector[detNo]->GetEveGeoShape());
-	             }
-	            }
+      ls->AddLine(c2.GetCoordinate(1).x(), c2.GetCoordinate(1).y(), c2.GetCoordinate(1).z(), c2.GetCoordinate(2).x(),
+                  c2.GetCoordinate(2).y(), c2.GetCoordinate(2).z());
+      ls->AddMarker(c2.GetCoordinate(1).x(), c2.GetCoordinate(1).y(), c2.GetCoordinate(1).z());
+      ls->AddMarker(c2.GetCoordinate(2).x(), c2.GetCoordinate(2).y(), c2.GetCoordinate(2).z());
+      ls->AddMarker(c2.GetCoordinate(3).x(), c2.GetCoordinate(3).y(), c2.GetCoordinate(3).z());
 
-	            for(int detNo = Tomography::SetupManager::instance()->GetLowerLayerStartIndex() ; detNo < Tomography::SetupManager::instance()->GetDetectorVector("GLASS").size() ; detNo++){
-	            //for(int detNo = 0 ; detNo < Tomography::SetupManager::instance()->GetLowerLayerStartIndex() ; detNo++){
-	            	              temp = c2.GetStripCoordinate(detNo-Tomography::SetupManager::instance()->GetLowerLayerStartIndex()+1);
-	            	              temp.Print();
-	            	              m.SetDx(temp.x());
-	            	              m.SetDy(temp.y());
-	            	              m.SetDz(temp.z());
+      ls->SetMarkerSize(1.3);
+      ls->SetMarkerStyle(4);
 
-	            	            //m.SetDx(*temp);
-	            				//m.SetDy(*(temp + 1));
-	            				//m.SetDz(*(temp + 2));
+      Tracking::Singleton::instance()->AddElement(ls);
 
-	            	            //Add some element
-	            	            if(gEve){
-	            	              hittedPixelVector.push_back(new HittedPixel(m));
-	            	              Tracking::Singleton::instance()->AddElement(hittedPixelVector[detNo]->GetEveGeoShape());
+      std::cout << "Angle : "
+                << l.CalculateAngle(c.GetCoordinate(1), c.GetCoordinate(2), c2.GetCoordinate(1), c2.GetCoordinate(2))
+                << std::endl;
+      sleep(2);
+      Tracking::Singleton::instance()->RemoveElement();
+    }
+  }
 
-	            	             }
-	            	            }
-
-
-	            std::cout<<"Pt : ";
-	            c.GetCoordinate(1).Print();
-	            TEveStraightLineSet *ls = new TEveStraightLineSet();
-	            ls->AddLine( c.GetCoordinate(1).x(),c.GetCoordinate(1).y(),c.GetCoordinate(1).z(),
-	            		     c.GetCoordinate(3).x(),c.GetCoordinate(3).y(),c.GetCoordinate(3).z());
-
-
-	           ls->AddLine( c2.GetCoordinate(1).x(),c2.GetCoordinate(1).y(),c2.GetCoordinate(1).z(),
-	              	         c2.GetCoordinate(2).x(),c2.GetCoordinate(2).y(),c2.GetCoordinate(2).z());
-
-
-	            ls->SetMarkerSize(1.5);
-	            ls->SetMarkerStyle(4);
-
-	            Tracking::Singleton::instance()->AddElement(ls);
-
-	           std::cout<<"Angle : "<< l.CalculateAngle(c.GetCoordinate(1), c.GetCoordinate(2), c2.GetCoordinate(1), c2.GetCoordinate(2))
-	        		   << std::endl;
-	           sleep(2);
-	           Tracking::Singleton::instance()->RemoveElement();
-
-
-	        }
-	        }
-
-	        void RunThread() {
-	          TThread *mythread = new TThread("My Thread", (void (*)(void *)) & Update::handle, (void *)this);
-	          mythread->Run();
-	        }
-
-
+  void RunThread() {
+    TThread *mythread = new TThread("My Thread", (void (*)(void *)) & Update::handle, (void *)this);
+    mythread->Run();
+  }
 };
-
 }
 
 #endif
--- TemplatizedInterface/test/GlassRpc_test.cpp
+++ TemplatizedInterface/test/GlassRpc_test.cpp
@@ -39,24 +39,28 @@ int main(){
   channelVector.push_back(32);
 
   std::cout<<"===========================================================================" << std::endl;
+  Detector *rpc0 = new GlassRpc(3,"SecondGlassRpc", 120,-1);
   Detector *rpc1 = new GlassRpc(3,"SecondGlassRpc", 90,-1);
   Detector *rpc2 = new GlassRpc(3,"SecondGlassRpc", 60,-1);
   Detector *rpc3 = new GlassRpc(3,"SecondGlassRpc", 30,63);
 
-  Detector *rpc5 = new GlassRpc(4,"SecondGlassRpc", -30,-1);
-  Detector *rpc6 = new GlassRpc(4,"SecondGlassRpc", -60,-1);
-  Detector *rpc4 = new GlassRpc(4,"SecondGlassRpc", -90,-1);
+  Detector *rpc4 = new GlassRpc(4,"SecondGlassRpc", -30,-1);
+  Detector *rpc5 = new GlassRpc(4,"SecondGlassRpc", -60,-1);
+  Detector *rpc6 = new GlassRpc(4,"SecondGlassRpc", -90,-1);
+  Detector *rpc7 = new GlassRpc(4,"SecondGlassRpc", -120,-1);
 
   Print(rpc2);
+  v.Register(rpc0);
   v.Register(rpc1);
   v.Register(rpc2);
   v.Register(rpc3);
   v.Register(rpc4);
   v.Register(rpc5);
   v.Register(rpc6);
+  v.Register(rpc7);
 
-  Detector *topPlane = new TriggeringPlane(2,"TopPlane", -105,-1);
-  Detector *bottomPlane = new TriggeringPlane(2,"BottomPlane", 105,7);
+  Detector *topPlane = new TriggeringPlane(2,"TopPlane", -150,-1);
+  Detector *bottomPlane = new TriggeringPlane(2,"BottomPlane", 150,7);
 
   std::cout<<"This should be cms : " << rpc1->GetDetectorType() << std::endl;
   std::cout<<"This shoudl be glass " << rpc2->GetDetectorType() << std::endl;
@@ -67,12 +71,14 @@ int main(){
 
 
   //Tomography::SetupManager s;
+  Tomography::SetupManager::instance()->Register(rpc0);
   Tomography::SetupManager::instance()->Register(rpc1);
   Tomography::SetupManager::instance()->Register(rpc2);
   Tomography::SetupManager::instance()->Register(rpc3);
   Tomography::SetupManager::instance()->Register(rpc4);
   Tomography::SetupManager::instance()->Register(rpc5);
   Tomography::SetupManager::instance()->Register(rpc6);
+  Tomography::SetupManager::instance()->Register(rpc7);
   //Tomography::SetupManager::instance()->RunThread();
   Update u;
   u.RunThread();
