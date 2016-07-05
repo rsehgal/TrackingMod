fe946c5923b4431e5ddf4f31d13ad0774449abc8
Raman Sehgal <raman.sehgal@cern.ch>
2016-07-05 18:20:53 +0530

    Strange issue with Tracks when filling them in tracksVector. Within first loop it works fine, but if checkout in next loop then it somehow gave same values. NEEDS A FIX ASAP.

--- TemplatizedInterface/test/core/TestPOCA.cpp
+++ TemplatizedInterface/test/core/TestPOCA.cpp
@@ -16,6 +16,7 @@
 #include <TGeoShape.h>
 #include <TGeoBBox.h>
 #include "Paddle.h"
+#include "Imaging.h"
 typedef Tomography::Properties Detector;
 using namespace Tomography;
 int main(){
@@ -50,21 +51,30 @@ int main(){
     setup->Register(paddle);
 
     TGeoShape *scatterer = new TGeoBBox("TestScatterer", 5.,5.,5.);
-    int numOfTracks = 100;
+    int numOfTracks = 10;
     Tomography::SimulateScatteredTracks s(scatterer,"GLASS");
     std::vector<Track*> incomingTracksVector;
     std::vector<Track*> outgoingTracksVector;
 
+    incomingTracksVector.reserve(numOfTracks);
+    outgoingTracksVector.reserve(numOfTracks);
+
     for(int trkNo = 0 ; trkNo < numOfTracks ; trkNo++){
     s.GenerateIncomingTrack();
     s.GenerateOutgoingTrack();
+    std::cout<<"--- Scattering point : " ; s.GetScatteringPoint().Print();
     v.Register(s.GetIncomingTrack());
+
     incomingTracksVector.push_back(s.GetIncomingTrack());
     outgoingTracksVector.push_back(s.GetOutgoingTrack());
 
+
+    std::cout<<"Incoming track : ";
+    incomingTracksVector[trkNo]->GetP1().Print() ;
+    outgoingTracksVector[trkNo]->GetP1().Print();
+
     v.RegisterLine(s.GetIncomingTrack()->GetP2(),s.GetScatteringPoint());
     v.RegisterLine(s.GetScatteringPoint(),s.GetOutgoingTrack()->GetP1());
-
     v.Register(s.GetOutgoingTrack());
     //numOfTracks--;
     }
@@ -73,14 +83,48 @@ int main(){
     //v.RegisterLine(hitVector[0], hitVector[hitVector.size()-1]);
 
 
+    Tracking::ImageReconstruction im;
+    Tracking::Vector3D<double> pocaPt;
+    double error = 1e-13;
+    double minz=0., maxz=0.;
+
+    incomingTracksVector[0]->GetP1().Print();
+    incomingTracksVector[1]->GetP1().Print();
+
+
     //Printing Coordinates of incoming and outgoing tracks
+/*
+    int mismatchCount = 0;
     for(int i=0 ; i < numOfTracks ; i++){
     	std::cout<<"P1 for IncomingTrack : "; incomingTracksVector[i]->GetP1().Print();
     	std::cout<<"P2 for IncomingTrack : "; incomingTracksVector[i]->GetP2().Print();
         std::cout << "P1 for OutgoingTrack : "; outgoingTracksVector[i]->GetP1().Print();
         std::cout << "P2 for OutgoingTrack : "; outgoingTracksVector[i]->GetP2().Print();
         std::cout<<"---------------------------------------------" << std::endl;
+        Tracking::Vector3D<double> p1(0.,0.,0.), q1(0.,0.,0.);
+        pocaPt = im.POCA(incomingTracksVector[i]->GetP1(),
+        		         incomingTracksVector[i]->GetDirCosine(),
+						 outgoingTracksVector[i]->GetP1(),
+						 outgoingTracksVector[i]->GetDirCosine(),p1,q1);
+
+        Tracking::Vector3D<double> diff = pocaPt - s.GetScatteringPoint();
+
+        if(std::fabs(diff.x()) > error || std::fabs(diff.y()) > error || std::fabs(diff.z()) > error ){
+        	mismatchCount++;
+        }
+
+        std::cout<<"POCA POINT : "; pocaPt.Print();
+
+        if(pocaPt.z() < minz)
+        	minz = pocaPt.z();
+        if(pocaPt.z() > maxz)
+        	maxz = pocaPt.z();
+
     }
+
+    std::cout<<"Total Num of mismatched Points : " << mismatchCount << std::endl;
+    std::cout<<"MinZ : "<< minz <<" : MaxZ : "<< maxz << std::endl;
+*/
 
 	v.Show();
 	gEve->DoRedraw3D();
