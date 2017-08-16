5ec4746143c7a6c5bc894aaafae4d6985f9037c6
Raman Sehgal <raman.sehgal@cern.ch>
2017-08-16 11:37:18 +0530

    Added very basic Simulation, which is very useful for debugging. This setup contain 4 RPC and one lead block in between, and the output shows where the primary muons hit the detectors.

---
+++ SimulationBasic/src/B1EventAction.cc
@@ -0,0 +1,91 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: B1EventAction.cc 75117 2013-10-28 09:38:37Z gcosmo $
+//
+/// \file B1EventAction.cc
+/// \brief Implementation of the B1EventAction class
+
+#include "B1EventAction.hh"
+#include "B1Run.hh"
+
+#include "G4Event.hh"
+#include "G4RunManager.hh"
+//#include "Tree.h"
+#include "B1RunAction.hh"
+//#include <TVector3.h>
+
+//Trying to use new Architecture
+//#include "EventHelper.h"
+//#include "Voxelator_Evolution.h"
+//#include "Imaging.h"
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+int B1EventAction::evNo = 0;
+//int B1EventAction::evMultiplicity = 0;
+//int B1EventAction::genuineEventCounter = 0;
+//std::vector<G4String> B1EventAction::volName({"Hello"});
+//std::vector<G4double> B1EventAction::energy({0.});
+//std::vector<G4double> B1EventAction::vertexEnergy({0.});
+std::vector<G4ThreeVector> B1EventAction::position({G4ThreeVector(0.,0.,0.)});
+
+B1EventAction::B1EventAction()
+: G4UserEventAction(),
+  fEdep(0.)
+{} 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1EventAction::~B1EventAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1EventAction::BeginOfEventAction(const G4Event*)
+{ evNo++;
+if(!(evNo%10000))
+  std::cout << "========  " << evNo << "  Events over... =======" << std::endl;
+  position.clear();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1EventAction::EndOfEventAction(const G4Event*)
+{  
+  // accumulate statistics in B1Run
+  B1Run* run 
+    = static_cast<B1Run*>(
+        G4RunManager::GetRunManager()->GetNonConstCurrentRun());
+  run->AddEdep(fEdep);
+ // Tracking::Tree::instance()->Fill();
+ std::cout << "======== Event no : "<< evNo << "  ended  =======" << std::endl;
+
+  std::cout<<"-----------------------------------------------------" << std::endl;
+
+}
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
---
+++ SimulationBasic/src/B1SteppingAction.cc
@@ -0,0 +1,96 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: B1SteppingAction.cc 74483 2013-10-09 13:37:06Z gcosmo $
+//
+/// \file B1SteppingAction.cc
+/// \brief Implementation of the B1SteppingAction class
+
+#include "B1SteppingAction.hh"
+#include "B1EventAction.hh"
+#include "MyDetectorConstruction.h"
+#include "G4Step.hh"
+#include "G4Event.hh"
+#include "G4RunManager.hh"
+#include "G4LogicalVolume.hh"
+//#include "Tree.h"
+#include "B1RunAction.hh"
+//#include <experimental/random>
+#include <ctime>
+#include <cstdlib>
+#include "base/Global.h"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+int B1SteppingAction::stepNum = -1;
+
+B1SteppingAction::B1SteppingAction(B1EventAction* eventAction)
+: G4UserSteppingAction(),
+  fEventAction(eventAction),
+  fScoringVolume(0)
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1SteppingAction::~B1SteppingAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1SteppingAction::UserSteppingAction(const G4Step* step)
+{
+stepNum++;
+
+  G4double edepStep = step->GetTotalEnergyDeposit();
+  fEventAction->AddEdep(edepStep);  
+
+  G4StepPoint *point1 = step->GetPreStepPoint();
+  G4StepPoint *point2 = step->GetPostStepPoint();
+  G4TouchableHandle touch1 = point1->GetTouchableHandle();
+  G4TouchableHandle touch2 = point2->GetTouchableHandle();
+
+  G4Track* track = step->GetTrack();
+  G4ThreeVector hitPre = track->GetPosition();
+  G4ThreeVector hitPost = track->GetPosition();
+  G4ThreeVector hitBeginOfPost = track->GetPosition();
+
+
+
+  {
+
+	  if(touch1->GetVolume()->GetName().substr(0,3) == "RPC"
+			  && point1->GetStepStatus()==fGeomBoundary &&
+			  track->GetDefinition()->GetParticleName().substr(0,2)=="mu"){
+			 B1EventAction::position.push_back(point1->GetPosition());
+			 std::cout << step->GetTrack()->GetDefinition()->GetParticleName() << std::endl;
+			 std::cout<<"Muon Hit the : " << touch1->GetVolume()->GetName() << std::endl;
+	  }
+ 
+  }
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
