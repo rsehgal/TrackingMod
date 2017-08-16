50f19fdcb2309fb71a98a7b560e4e30d4d1ad88c
Raman Sehgal <raman.sehgal@cern.ch>
2017-08-16 11:37:18 +0530

    Added very basic Simulation, which is very useful for debugging. This setup contain 4 RPC and one lead block in between, and the output shows where the primary muons hit the detectors.

---
+++ SimulationBasic/src/B1RunAction.cc
@@ -0,0 +1,148 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: B1RunAction.cc 87359 2014-12-01 16:04:27Z gcosmo $
+//
+/// \file B1RunAction.cc
+/// \brief Implementation of the B1RunAction class
+
+#include "B1RunAction.hh"
+#include "MyPrimaryGeneratorAction.h"
+#include "MyDetectorConstruction.h"
+#include "B1Run.hh"
+
+#include "G4RunManager.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4LogicalVolume.hh"
+#include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
+#include <G4FastStep.hh>
+#include <string.h>
+#include "B1EventAction.hh"
+#include <vector>
+
+
+using namespace std;
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+double B1RunAction::fScatteringAngle = 0;
+
+B1RunAction::B1RunAction()
+: G4UserRunAction()
+{ 
+  // add new units for dose
+  //
+  const G4double milligray = 1.e-3*gray;
+  const G4double microgray = 1.e-6*gray;
+  const G4double nanogray  = 1.e-9*gray;  
+  const G4double picogray  = 1.e-12*gray;
+   
+  new G4UnitDefinition("milligray", "milliGy" , "Dose", milligray);
+  new G4UnitDefinition("microgray", "microGy" , "Dose", microgray);
+  new G4UnitDefinition("nanogray" , "nanoGy"  , "Dose", nanogray);
+  new G4UnitDefinition("picogray" , "picoGy"  , "Dose", picogray);        
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1RunAction::~B1RunAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4Run* B1RunAction::GenerateRun()
+{
+  return new B1Run; 
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1RunAction::BeginOfRunAction(const G4Run*)
+{
+  //inform the runManager to save random number seed
+  G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1RunAction::EndOfRunAction(const G4Run* run)
+{
+  G4int nofEvents = run->GetNumberOfEvent();
+  if (nofEvents == 0) return;
+  
+  const B1Run* b1Run = static_cast<const B1Run*>(run);
+
+  // Compute dose
+  //
+  G4double edep  = b1Run->GetEdep();
+  G4double edep2 = b1Run->GetEdep2();
+  G4double rms = edep2 - edep*edep/nofEvents;
+  if (rms > 0.) rms = std::sqrt(rms); else rms = 0.;
+
+  const MyDetectorConstruction* detectorConstruction
+   = static_cast<const MyDetectorConstruction*>
+     (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
+
+  const MyPrimaryGeneratorAction* generatorAction
+   = static_cast<const MyPrimaryGeneratorAction*>
+     (G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
+  G4String runCondition;
+  // Print
+  //  
+  if (IsMaster()) {
+    G4cout
+     << G4endl
+     << "--------------------End of Global Run-----------------------";
+  }
+  else {
+    G4cout
+     << G4endl
+     << "--------------------End of Local Run------------------------";
+  }
+
+  ofstream fs("run.txt");
+  fs << 3000 << " " << "G4_Fe" << " " << 100 << " ";
+  for(int i = 0 ; i < b1Run->GetScatteringAngleVector().size() ; i++){
+	  fs << b1Run->GetScatteringAngleVector()[i] << " " ;
+  }
+  fs.close();
+
+
+
+
+/*
+  Tomography::RunHelper *runHelper = new Tomography::RunHelper();
+  //Now trying to calculate Radiation for the whole run
+  std::cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << std::endl;
+  std::cout<<"                              SD : " << CommonFunc::Functions::StandardDeviation(runHelper->GetScatteringAngleVector()) << std::endl;
+  std::cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << std::endl;
+  std::cout<<"                              RL : " << CommonFunc::Functions::RadiationLength(runHelper->GetScatteringAngleVector(),10) << std::endl;
+  std::cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" << std::endl;
+*/
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
---
+++ SimulationBasic/src/MyDetectorConstruction.cpp
@@ -0,0 +1,105 @@
+#include "MyDetectorConstruction.h"
+#include "G4RunManager.hh"
+#include "G4NistManager.hh"
+#include "G4Box.hh"
+#include "G4Cons.hh"
+#include "G4Orb.hh"
+#include "G4Sphere.hh"
+#include "G4Trd.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4Isotope.hh"
+#include "G4Element.hh"
+#include "G4Material.hh"
+#include "G4UnitsTable.hh"
+
+MyDetectorConstruction::MyDetectorConstruction(){
+
+}
+
+MyDetectorConstruction::~MyDetectorConstruction(){}
+
+G4VPhysicalVolume* MyDetectorConstruction::Construct(){
+  
+  G4NistManager* nist = G4NistManager::Instance();
+  G4bool checkOverlaps = true;
+
+  
+  // Envelope params
+  //
+  G4double env_sizeXY = 20*m, env_sizeZ = 30*m;
+  //G4Material* env_mat = nist->FindOrBuildMaterial("G4_WATER");
+  
+  //     
+  // World
+  //
+  G4double world_sizeXYZ = 200*cm;
+  G4double world_sizeXY = 1.2*env_sizeXY;
+  G4double world_sizeZ  = 1.2*env_sizeZ;
+
+  G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
+  G4Material *galactic = nist->FindOrBuildMaterial("G4_Galactic");
+
+  G4Box* solidWorld =    
+    new G4Box("World",                       //its name
+       0.5*world_sizeXYZ, 0.5*world_sizeXYZ, 0.5*world_sizeXYZ);     //its size
+      
+  G4LogicalVolume* logicWorld =                         
+    new G4LogicalVolume(solidWorld,          //its solid
+                        world_mat,           //its material
+                        "World");            //its name
+                                   
+  G4VPhysicalVolume* physWorld = 
+    new G4PVPlacement(0,                     //no rotation
+                      G4ThreeVector(),       //at (0,0,0)
+                      logicWorld,            //its logical volume
+                      "World",               //its name
+                      0,                     //its mother  volume
+                      false,                 //no boolean operation
+                      0,                     //copy number
+                      checkOverlaps);        //overlaps checking
+
+ 
+  //Adding RPC layer, considering that it is made up of galactic, to avoid interaction in RPC itself
+  G4Box *rpc = new G4Box("RPC",50.*cm,50.*cm,0.5*cm);
+  G4LogicalVolume *logicalRPC = new G4LogicalVolume(rpc,galactic,"LogicalRPC");
+  int rpcNum = -1;
+	for (int k = 0; k < 8; k++) {
+		if (k == 0 || k == 3 || k == 4 || k == 7)
+			continue;
+		rpcNum++;
+		G4VPhysicalVolume *planeEPhy = new G4PVPlacement(0,
+														 G4ThreeVector(0., 0., (-105 + 30 * k) * cm),
+														 logicalRPC,
+														 "RPC" + std::to_string(rpcNum),
+														 logicWorld,
+														 false,
+														 rpcNum,
+														 checkOverlaps);
+	}
+
+
+
+
+  //Lets try to build material from NIST database
+  G4Box *leadBlock = new G4Box("LeadBlock",5.*cm,5.*cm,15.*cm);
+  G4Material *Pb=nist->FindOrBuildMaterial("G4_Pb");
+  G4LogicalVolume *logicalLeadBlock = new G4LogicalVolume(leadBlock,Pb,"LogicalLeadBlock");
+  G4VPhysicalVolume *phyLeadBlock = new G4PVPlacement(0,
+                            //G4ThreeVector(),
+                            G4ThreeVector(),
+                            logicalLeadBlock,
+                            "PhysicalWorld",
+                            logicWorld,
+                            false,
+                            0,
+                            checkOverlaps);
+
+
+
+
+	return physWorld;
+
+}
