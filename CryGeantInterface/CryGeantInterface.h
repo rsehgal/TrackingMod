ff983e1cb1005f40d4e31ed8fd5ade781bd5d73f
Raman Sehgal <raman.sehgal@cern.ch>
2018-01-10 17:32:35 +0530

    Added a directory CryGeantInterface.
    1)This directory contains code to provide interface between cry and geant,
      hence allow to use CRY easily with geant4.
    
    2)On compilation it will create a library "libcryg4interface.so".
      copy this library to installed CRY library folder.
    
    3)Also copy the corresponding header files in this directory
      to the "inc" directory of installed CRY.

---
+++ CryGeantInterface/CryGeantInterface.h
@@ -0,0 +1,51 @@
+/*
+ * CryGeantInterface.h
+ *
+ *  Created on: Jan 1, 2018
+ *      Author: rsehgal
+ */
+
+#ifndef CRYGEANTINTERFACE_CRYGEANTINTERFACE_H_
+#define CRYGEANTINTERFACE_CRYGEANTINTERFACE_H_
+
+#include "G4ParticleGun.hh"
+#include "globals.hh"
+#include "G4ThreeVector.hh"
+#include "G4DataVector.hh"
+#include "G4ParticleTable.hh"
+#include "Randomize.hh"
+#include "CRYSetup.h"
+#include "CRYGenerator.h"
+#include "CRYParticle.h"
+#include "CRYUtils.h"
+#include "RNGWrapper.hh"
+#include "PrimaryGeneratorMessenger.hh"
+#include "G4Event.hh"
+
+#include "G4SystemOfUnits.hh"
+
+
+class CryGeantInterface {
+public:
+	CryGeantInterface();
+	virtual ~CryGeantInterface();
+	CryGeantInterface(const char *inputfile);
+	void ForCry(const char *inputfile);
+	void GeneratePrimariesForCry(G4Event *anEvent);
+	void InputCRY();
+	void UpdateCRY(std::string* MessInput);
+	void CRYFromFile(G4String newValue);
+	const G4ParticleGun* GetParticleGun() const { return particleGun; }
+
+private:
+	    //G4ParticleGun*  fParticleGun; // pointer a to G4 gun class
+	    std::vector<CRYParticle*> *vect; // vector of generated particles
+	    G4ParticleTable* particleTable;
+	    G4ParticleGun* particleGun;
+	    CRYGenerator* gen;
+	    G4int InputState;
+	    PrimaryGeneratorMessenger* gunMessenger;
+
+};
+
+#endif /* CRYGEANTINTERFACE_CRYGEANTINTERFACE_H_ */
