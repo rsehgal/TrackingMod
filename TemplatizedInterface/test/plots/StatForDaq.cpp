e5e559b6d9ac09642c0ef8f0bc7cca7c5e8fde81
Raman Sehgal <raman.sehgal@cern.ch>
2018-02-15 19:11:57 +0530

    DetectorMapping Modified, a new function 'ReadDaqInfo()' added, that will read the <runnumber>.info file and <runnumber>.root, and get the voltage, temp, pressure, humidity, EffTop, EffBottom, EffBoth. This information needs to be written to a file, which will be consumed by EfficiencyPlotter code.

--- TemplatizedInterface/test/plots/StatForDaq.cpp
+++ TemplatizedInterface/test/plots/StatForDaq.cpp
@@ -22,6 +22,7 @@ using namespace Tomography;
 
 int main(int argc, char* argv[]) {
   std::string temp_str = std::to_string(atoi(argv[1]));
+  std::string daqinfofile = temp_str;
   temp_str += ".root";
   TApplication *fApp = new TApplication("Test", NULL, NULL);
   Tracking::Tree::instance()->ReadTree(temp_str.c_str(), "BSC_DATA_TREE", 0);
@@ -32,12 +33,16 @@ int main(int argc, char* argv[]) {
   std::vector<int> moduleVector = detectorMap->GetModuleVector();
   std::vector<double> zcoordinateVector = detectorMap->GetZCoordinateVector();
 
-  for(int i = 0 ; i < detectorNamesVector.size(); i++){
+  for(int i = 0 ; i < detectorNamesVector.size()-1; i++){
   		std::cout << "Name of Detector : " << (i+1) << " : " << detectorNamesVector[i] << std::endl;
   		Detector *rpc = new GlassRpc(moduleVector[i], detectorNamesVector[i], zcoordinateVector[i], startChannelVector[i]);
   		rpc->GetX_Y_And_ClusterHistograms();
   		rpc->GetStripProfile();
   		delete rpc;
   	}
+
+  //daqinfofile += ".info";
+  detectorMap->ReadDaqInfo(daqinfofile);
+  detectorMap->PrintEfficiencyVector();
   fApp->Run();
 }
