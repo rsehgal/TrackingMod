b79b257fd89bfdee4d7289f1c812b5b66bf8dc8e
Raman Sehgal <raman.sehgal@cern.ch>
2018-03-08 17:59:08 +0530

    Lot of work is done for the implementation of Gaussian Mixture Model. Also added the corresponding test like TestGmm.cpp, TestGaussian.cpp.
    Vector3D also modified to store probability that a point belongs to
    a particular Gaussian.

--- gmm/include/GMM.h
+++ gmm/include/GMM.h
@@ -8,11 +8,41 @@
 #ifndef GMM_INCLUDE_GMM_H_
 #define GMM_INCLUDE_GMM_H_
 
+#include <vector>
+#include <Eigen/Dense>
+#include "base/Vector3D.h"
+
 namespace Tomography {
 
+class Gaussian;
+
 class GMM {
+private:
+	unsigned int fNumOfGaussians;
+	unsigned int fDimension;
+
+	std::vector<Tracking::Vector3D<double>> fPointVector;
+
+	//size of these should be equal to the number of gaussians used in gmm
+	//std::vector<Eigen::VectorXd> fMeanVector;
+	//std::vector<Eigen::MatrixXd> fCovarsVector;
+
+	//Lets have a std::vector of Gaussian.
+	std::vector<Gaussian*> fGaussianVector;
+
+
+	Eigen::MatrixXd InitializeCovars();
+	Eigen::VectorXd InitializeMean();
+
 public:
 	GMM();
+	GMM(int numOfGaussians, int dimension);
+
+	int GetNumOfGaussians() const {return fNumOfGaussians;}
+	std::vector<Gaussian*> GetGaussianVector() const {return fGaussianVector;}
+
+	void DoExpectation();
+	void DoMaximization();
 	virtual ~GMM();
 };
 
