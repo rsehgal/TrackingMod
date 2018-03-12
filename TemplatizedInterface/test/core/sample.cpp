702a1b2aa4fbaa0bd3e9bf6d6fe7e914372c7069
Raman Sehgal <raman.sehgal@cern.ch>
2018-03-12 17:51:42 +0530

    First version of Gaussian and GMM in 2 Dimension, For the time being implemented as a separate class like Gaussian1D and GMM1D. Also added the corresponding test TestGmm2D.cpp Also aded a sample data file gauss2D.txt, that contains data for two gaussian.

--- TemplatizedInterface/test/core/sample.cpp
+++ TemplatizedInterface/test/core/sample.cpp
@@ -5,20 +5,43 @@ int main(){
   std::ofstream outfile("gauss.txt");
   double val = 0.;
   TRandom3 r;
+
+  int dim = 2;
+  if(dim == 1){
   for(int i = 0 ; i < 20000 ; i++){
 	
-	val = r.Gaus(5.,4.);
+	val = r.Gaus(12.,2.);
         //std::cout << val << std::endl;
 	outfile << val << std::endl;
   }
+  }else{
+  double meanX = 12. , meanY = 10.;
+  for(int i = 0 ; i < 20000 ; i++){
 
+	double valX = r.Gaus(meanX,2.);
+	double valY = r.Gaus(meanY,2.);
+        //std::cout << val << std::endl;
+	outfile << valX << "  " << valY << std::endl;
+  }
+
+  meanX = 0.; meanY = 0.;
+  for(int i = 0 ; i < 30000 ; i++){
+
+	double valX = r.Gaus(meanX,2.);
+	double valY = r.Gaus(meanY,2.);
+        //std::cout << val << std::endl;
+	outfile << valX << "  " << valY << std::endl;
+  }
+
+  }
+/*
   for(int i = 0 ; i < 35000 ; i++){
   
   val = r.Gaus(8.,2.);
         //std::cout << val << std::endl;
   outfile << val << std::endl;
   }
-
+*/
 
   outfile.close();
   return 0;
