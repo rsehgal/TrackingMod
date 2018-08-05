6ac488adb082ee6f21383589fd987c3cf520dbb8
Raman Sehgal <raman.sehgal@cern.ch>
2018-08-05 20:47:23 +0200

    Modified eventaction to also store incoming, outgoing, fittedIncoming and fittedOutgoing muon angles.

--- TemplatizedInterface/test/plots/GeneratorStats.cpp
+++ TemplatizedInterface/test/plots/GeneratorStats.cpp
@@ -9,7 +9,7 @@ int main(){
 
     TApplication *fApp = new TApplication("Test", NULL, NULL);
     TCanvas *c = new TCanvas("TestCanvas", "ScatteringAngleCanvas", 800, 600);
-    c->Divide(2,2);
+    c->Divide(3,2);
     TH1F *muonAngle = new TH1F("AngularDistribution From CRY","AngularDistribution From CRY",100,0.,M_PI/2.);
     TH1F *muonEnergy = new TH1F("EnergyDistribution From CRY","EnergyDistribution From CRY",100,-100.,10000.);
     std::ifstream infile("StatsFromGenerator.txt");
@@ -24,10 +24,14 @@ int main(){
     std::ifstream infile2("StatsFromEventAction.txt");
     TH1F *muonAngleEventAction = new TH1F("AngularDistribution From EventAction","AngularDistribution From EventAction",100,0.,M_PI/2.);
     TH1F *muonEnergyEventAction = new TH1F("EnergyDistribution From EventAction","EnergyDistribution From EventAction",100,-100.,10000.);
-    double incoming = 0., outgoing = 0., diff = 0., ener = 0;
+    TH1F *fittedIncomingMuonAngle = new TH1F("Fitted Incoming AngularDistribution","Fitted Incoming AngularDistribution",100,0.,M_PI/2.);
+    TH1F *fittedOutgoingMuonAngle = new TH1F("Fitted Outgoing AngularDistribution","Fitted Outgoing AngularDistribution",100,0.,M_PI/2.);
+    double incoming = 0., outgoing = 0., diff = 0., ener = 0, incomingFitted = 0., outgoingFitted = 0., diffFitted = 0.;
     while(!infile2.eof()){
-        infile2 >> incoming >> outgoing >> diff >> ener ; 
+        infile2 >> incoming >> outgoing >> diff >> incomingFitted >> outgoingFitted >> diffFitted >>  ener ; 
         muonAngleEventAction->Fill(incoming);
+        fittedIncomingMuonAngle->Fill(incomingFitted);
+        fittedOutgoingMuonAngle->Fill(outgoingFitted);
         if(energy > 10)
             muonEnergyEventAction->Fill(ener);
     }
@@ -40,6 +44,10 @@ int main(){
     muonAngleEventAction->Draw();
     c->cd(4);
     muonEnergyEventAction->Draw();
+    c->cd(5);
+    fittedIncomingMuonAngle->Draw();
+    c->cd(6);
+    fittedOutgoingMuonAngle->Draw();
     fApp->Run();
 
     return 0;
