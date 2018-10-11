d168f5e0f75fba49fce4da8b9ffb2df31da97e62
Raman Sehgal <sc.ramansehgal@gmail.com>
2018-10-11 09:56:37 +0530

    Cleanup in TestVoxelVisualizatoin.cpp

--- TemplatizedInterface/test/visualization/TestVoxelVisualization.cpp
+++ TemplatizedInterface/test/visualization/TestVoxelVisualization.cpp
@@ -15,87 +15,40 @@
 
 using Tomography::VisualizationHelper;
 using Vec_t = Tracking::Vector3D<double>;
-int main(int argc, char *argv[]){
-    TApplication *fApp = new TApplication("Test", NULL, NULL);
-    //VisualizationHelper v;
-    Tomography::VisualizationHelper *v = Tomography::VisualizationHelper::instance();
-    //Vec_t pt(3.,4.,5.,10.);
-    v->InitializeVisualizer();
-    //v.Register(pt);
-    gStyle->SetPalette(kRainBow);
+int main(int argc, char *argv[]) {
+	TApplication *fApp = new TApplication("Test", NULL, NULL);
+	//VisualizationHelper v;
+	Tomography::VisualizationHelper *v =
+			Tomography::VisualizationHelper::instance();
+	v->InitializeVisualizer();
+	gStyle->SetPalette(kRainBow);
 
 //Reading Voxels.txt
-#if(1)
-std::ifstream filehandle(argv[1]);
-int numOfEvents = atoi(argv[2]);
-double x = 0., y = 0., z = 0.;
-double color = 0.;
-double intensity = 0.;
-filehandle >> x >> y >> z >> color;
-Vec_t voxDim(x,y,z);
 
-/*filehandle >> x >> y >> z >> color;
-Vec_t incomingHitPoint(x,y,z,color);
+	std::ifstream filehandle(argv[1]);
+	int numOfEvents = atoi(argv[2]);
+	double x = 0., y = 0., z = 0.;
+	double colorSD = 0.;
+	double rl = 0.;
+	double intensity = 0.;
+	filehandle >> x >> y >> z >> colorSD;
+	Vec_t voxDim(x, y, z);
 
+	int i = 0;
 
-filehandle >> x >> y >> z >> color;
-Vec_t outgoingHitPoint(x,y,z,color);
+	while (numOfEvents) {
+		i++;
+		filehandle >> x >> y >> z >> colorSD >> rl;
+		std::cout << x << " , " << y << " , " << z << " , " << colorSD << " , "
+				<< rl << std::endl;
+		Vec_t voxCenter(x, y, z);
+		v->Register(voxDim, voxCenter, colorSD * 1000);
+		numOfEvents--;
+	}
 
-filehandle >> x >> y >> z >> color;
-Vec_t pocaPt(x,y,z,color);
-pocaPt.SetColor(6.);
+	v->Lock();
+	v->Show();
+	fApp->Run();
 
-
-Tomography::Track tr(incomingHitPoint,outgoingHitPoint);
-v->Register(incomingHitPoint);
-v->Register(outgoingHitPoint);
-v->Register(&tr,3);
-v->Register(pocaPt);
-
-*/
-int i = 0 ;
-
-while(numOfEvents){
-    i++;
-    filehandle >> x >> y >> z >> color >> intensity;
-    std::cout << x << " , " << y << " , " << z << " , " << color << " , " << intensity << std::endl;
-    //voxCenter.Set(x*1.,y*1.,z*1.);
-    Vec_t voxCenter(x,y,z);
-    v->Register(voxDim,voxCenter,intensity*10);
-    numOfEvents--;
-}
-
-
-#endif
-
-
-#if(0) // Demonstration
-    double color =10;
-    //Now tryig to register a voxel
-    Vec_t voxDim(100.,100.,90.);
-
-    for(int i = 0 ;  i < 4 ; i++){
-    Vec_t voxCenter(100.*i,0.,0.);
-    v->Register(voxDim,voxCenter,i+1);
-    }
-
-/*
-    voxCenter.Set(10.,0.,0.);
-    v.Register(voxDim,voxCenter,2);
-
-    voxCenter.Set(0.,10.,0.);
-    v.Register(voxDim,voxCenter,4);
-
-    voxCenter.Set(10.,10.,0.);
-    v.Register(voxDim,voxCenter,6);
-*/
-
-    
-    //rev.CloseBins();
-#endif
-    v->Lock();
-    v->Show();
-    fApp->Run();
-
-    return 0;
+	return 0;
 }
