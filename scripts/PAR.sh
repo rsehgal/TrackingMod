3523baa082f2cba31c08044863d13aa6ba6d49cd
Raman Sehgal <sc.ramansehgal@gmail.com>
2018-11-05 17:17:25 +0530

    Added script to Automatically generate PocaAccuracyRatio Graph PAR as function of material, for fixed number of events

--- scripts/PAR.sh
+++ scripts/PAR.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 rm -rf Par.txt
-for material in Al Fe #Pb U
+echo "numOfMaterials 4" > Par.txt
+echo "Al Fe Pb U" >> Par.txt
+numOfEvents=10000
+for material in Al Fe Pb U
 do
-	for numOfEvents in 1000 5000 #10000 15000 20000 30000 50000 100000
-	do
-		#echo "NumOfEvents : "$numOfEvents" :: Material : "$material
-		filename=$material"_"$numOfEvents".txt"
-		#echo $filename
-		sed "s/Material/$material/g" testMapping_Template.PAR | sed "s/NumOfEvents/$numOfEvents/g" > $filename
-		mv $filename testMapping.txt
-		rm -rf EventHelper* *RunHelper* Poca* 1234.root Fitted.root SampledFitted.root Exact.root Track* Hits.txt run.txt Stats* Actual* Hodoscope.gdml
-		./mainNew $1 
-		./OfflineRunEventHelper Exact	
-	done
-	echo "" >> Par.txt
+	filename=$material"_"$numOfEvents".txt"
+
+	#Creating testMapping.txt
+        sed "s/Material/$material/g" testMapping_Template.PAR | sed "s/NumOfEvents/$numOfEvents/g" > $filename
+        mv $filename testMapping.txt
+
+	#Deleting file that got created in last Run
+        rm -rf EventHelper* *RunHelper* Poca* 1234.root Fitted.root SampledFitted.root Exact.root Track* Hits.txt run.txt Stats* Actual* Hodoscope.gdml
+
+	#Runing the Simulation
+        ./mainNew $1 
+
+	#Running Offline Analysis
+        ./OfflineRunEventHelper Exact
+
 
 done
+
+#Creating the plots
+#./PlotPAR
