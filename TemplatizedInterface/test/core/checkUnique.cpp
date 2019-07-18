2e87f39889324e543c466e99a8601fdb25e5bf3e
Kavya <1198kavya@gmail.com>
2019-07-18 16:48:30 +0530

    Dupliacte values are identified

--- TemplatizedInterface/test/core/checkUnique.cpp
+++ TemplatizedInterface/test/core/checkUnique.cpp
@@ -18,8 +18,8 @@
 #include "TrackPredictor.h"
 
 using namespace std ;
-using Tomography::TrackPredictor;
 
+int globalCounter=0;
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct TestStruct{
@@ -29,17 +29,23 @@ struct TestStruct{
 
 struct treeNode
 {
+    int counter;
     string data;
     treeNode *left;
     treeNode *right;
     TestStruct t;
    
-    treeNode(){}
+    treeNode(){counter=0;}
 
     treeNode(string str){
+	counter=0;
         data = str;
     	t.data = data;
     }
+
+
+    //std::vector of string for this node
+    std::vector<std::string> strVector;
   
 
 };
@@ -51,19 +57,25 @@ struct treeNode
 
 
 
-
-
 treeNode *Insert(treeNode *node, string data)
 {
     if(node==NULL)
     {
         treeNode *temp = new treeNode(data);
+  	temp->strVector.push_back(data);
         //temp=new treeNode;
         //temp = (treeNode *)malloc(sizeof(treeNode));
         //temp -> data = data;
         temp -> left = temp -> right = NULL;
         return temp;
     }
+    if(data== (node->t.data)) 
+    { 
+        node->counter++; 
+	node->strVector.push_back(data);
+	//counter++;
+        return node; 
+    } 
     if(data >(node->t.data))
     {
         node->right = Insert(node->right,data);
@@ -72,9 +84,13 @@ treeNode *Insert(treeNode *node, string data)
     {
         node->left = Insert(node->left,data);
     }
+  
+    
     
     return node;
 }
+
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -85,17 +101,17 @@ treeNode * Find(treeNode *node, string data)
 {  
     int pos=0;
     if(node==NULL)
-    {   pos++;
+    {  
         return NULL;
     }
     if(data > node->t.data)
     {
-        pos++;
+       
         return Find(node->right,data); 				 /* Search in the right sub tree. */
     }
     else if(data < node->t.data)
     {
-        pos++;
+        
         return Find(node->left,data);				 /* Search in the left sub tree. */
     }
     else
@@ -122,6 +138,33 @@ void Preorder(treeNode *node)
 }
 
 
+void PrintDuplicateCount(treeNode *node)
+{
+   
+    
+    if(node==NULL)
+    {
+        return;
+    }
+
+    /*else{
+	std::cout <<"String  : " << node->data <<" : Count : " << node->counter << std::endl;
+    }
+    */
+    PrintDuplicateCount(node->left);
+    //cout<<node->t.data<<" " <<endl;
+    globalCounter += node->counter;
+    std::cout <<"String  : " << node->data <<" : Count : " << node->counter << std::endl;
+    if(node->strVector.size() > 1){
+    std::cout <<"============ Raman ============================"<<std::endl;
+    for(auto &str : node->strVector){
+	std::cout << str << std::endl;
+    }
+    std::cout <<"========================================"<<std::endl;
+    }
+    
+    PrintDuplicateCount(node->right);
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -133,15 +176,14 @@ void Preorder(treeNode *node)
 
 int main()
 {
-   TrackPredictor c;
+  
    std::string line; 
    std::string key;
    int count=0;
    int uni=0;
    string ch; 
    treeNode *root = NULL,*temp;
-   TestStruct t;
- 
+
    std::ifstream sfile("shacode.txt");
   
    std::vector<std::string> arr;
@@ -173,14 +215,14 @@ int main()
 
 
 				root= Insert(root,arr[0]);	
-				uni=1;
+				
 
 				for (int i = 1; i < arr.size(); i++) 
 				{
          			  
             
 					    Insert(root,arr[i]);
-					    uni++ ;
+					    
 		    
 					     if(!(i%10000))
            					 {    
@@ -189,11 +231,25 @@ int main()
 	                                          }
 				}
 
-       
+      //cout<<" The Number of duplicates : "<<counter<<endl; 
     cout<<"\n Elements in BST are:" <<endl;
-    Preorder(root);
-    cout<<" The Number of unique shacodes : "<<uni<<endl;
+    //Preorder(root);
+    PrintDuplicateCount(root);
+    cout<<" The Number of duplicates : "<< globalCounter<<endl; 
+    
             				
            
     return 0;
 }
+
+
+
+
+/*
+checkUnique.cpp will make a binary search tree from the data. 
+The data consists of shacodes from shacode.txt.
+This code will insert all the shacodes into the tree and do inorder traversal to print it . 
+It will check for unique shacodes .
+Currently alll the shacodes are unique and there is no duplicate value . 
+*/
+
