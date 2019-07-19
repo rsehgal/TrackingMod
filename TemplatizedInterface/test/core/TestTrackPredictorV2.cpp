d805ab2bff53a624ef12dc4ef5593ed69356a1fa
Kavya <1198kavya@gmail.com>
2019-07-19 15:51:51 +0530

    Modified TrackPreictorV2. Function Insert running succesfully

--- TemplatizedInterface/test/core/TestTrackPredictorV2.cpp
+++ TemplatizedInterface/test/core/TestTrackPredictorV2.cpp
@@ -7,11 +7,13 @@
 
 #include <iostream>
 #include "TrackPredictorV2.h"
+#include "PixelCombinationChecker.h"
 
 using Tomography::TrackPredictorV2;
 using Vec_t = Tracking::Vector3D<double>;
 
 int main(){
+
 	TrackPredictorV2 t;
 	Tomography::HitPointVector v1;
 	v1.push_back(Vec_t(249.71514449228925514, 312.07044576555085769, 1060));
@@ -21,6 +23,9 @@ int main(){
 	v1.push_back(Vec_t(363.92977063134026139, -78.804298120518140536, -740));
 	v1.push_back(Vec_t(382.96624272932518807, -143.94733838096792056, -1040));
 	t.Process("ABCD",v1);
+	//t.Process("ABCD",v1);
+
+	t.Print();
 
 }
 
