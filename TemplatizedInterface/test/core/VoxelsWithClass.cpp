a81ef49db0d1412e82329438c34591556596e200
Raman Sehgal <raman.sehgal@cern.ch>
2020-06-08 18:26:54 +0530

    Modified code to implement class % for individual voxel.

--- TemplatizedInterface/test/core/VoxelsWithClass.cpp
+++ TemplatizedInterface/test/core/VoxelsWithClass.cpp
@@ -14,11 +14,17 @@
 #include <fstream>
 #include "Stats.h"
 #include "base/Vector3D.h"
+#include <TH1F.h>
+#include <TApplication.h>
+#include <TStyle.h>
+
+
 
 using Tracking::Vector3D;
 
 int main(int argc, char *argv[]){
 
+	TApplication *fApp = new TApplication("Test", NULL, NULL);
 	std::string type = std::string(argv[1]);
 	int numOfParts = std::atoi(argv[2]);
 	double voxelSizeXY = (double)100*cm/numOfParts;
@@ -42,4 +48,25 @@ int main(int argc, char *argv[]){
 	outfile.close();
 	infile.close();
 
+	Tomography::EventHelper evHelper(fileToWrite);
+	//Tomography::evolution::Voxelator *voxelator=Tomography::evolution::Voxelator::instance();
+	std::vector<Tomography::Voxel_V2*> voxelVector = voxelator->GetVoxelVector();
+	std::cout << "===================================================" << std::endl;
+	std::cout<<"Size of Voxel Vector : " << voxelVector.size() << std::endl;
+	for(unsigned int i = 0 ; i < voxelVector.size() ; i++){
+
+		TH1F *h=voxelVector[i]->GetClassHist();
+		int binmax = h->GetMaximumBin();
+		//double x = h->GetXaxis()->GetBinCenter(binmax);
+		std::cout << "Num of PoCA points for index : "<< i << " : Voxel Number : " << voxelVector[i]->GetVoxelNum()
+				  << " : " << voxelVector[i]->GetPocaPointsVector().size()
+				  << " : Majority Class : " << binmax << std::endl;
+	}
+	voxelVector[0]->FillClassHist();
+	voxelVector[0]->GetClassHist()->Draw();
+
+
+	fApp->Run();
+
+	return 0;
 }
