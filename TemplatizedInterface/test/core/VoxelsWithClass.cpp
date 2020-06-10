457fa5ceb471b111b54ce4ae20b40c078f5abd26
Raman Sehgal <sc.ramansehgal@gmail.com>
2020-06-10 10:12:36 +0530

    Modified VoxelsWithClass.cpp to also store the voxels in Voxels.txt file

--- TemplatizedInterface/test/core/VoxelsWithClass.cpp
+++ TemplatizedInterface/test/core/VoxelsWithClass.cpp
@@ -34,19 +34,28 @@ int main(int argc, char *argv[]){
 
 	std::string fileToRead = type;
 	std::string fileToWrite = "ClassWithVoxels.txt";
+	std::string voxelFile = "Voxels.txt";
 
 	std::ifstream infile(fileToRead);
 	std::ofstream outfile(fileToWrite);
+	std::ofstream outVoxelFile(voxelFile);
 
 	while(!infile.eof()){
 		double x=0.,y=0.,z=0.,matClass=0.,exPar=0.;
 		infile >> x >> y >> z >> matClass >> exPar;
 		unsigned int voxelNum = voxelator->GetVoxelNumber(x,y,z);
+
 		outfile << x << " " <<  y << " " << z << " " <<  matClass << " " << voxelNum << std::endl;
+		
 	}	
 
 	outfile.close();
 	infile.close();
+
+	Vector3D<double> voxDim = voxelator->GetEachVoxelDim();
+	int col=1;
+	outVoxelFile  <<  voxDim.x() << " " <<  voxDim.y() << " " <<  voxDim.z() << " " <<  col << std::endl;
+
 
 	Tomography::EventHelper evHelper(fileToWrite);
 	//Tomography::evolution::Voxelator *voxelator=Tomography::evolution::Voxelator::instance();
@@ -54,18 +63,21 @@ int main(int argc, char *argv[]){
 	std::cout << "===================================================" << std::endl;
 	std::cout<<"Size of Voxel Vector : " << voxelVector.size() << std::endl;
 	for(unsigned int i = 0 ; i < voxelVector.size() ; i++){
-
+		Vector3D<double> voxCenter = voxelVector[i]->GetVoxelCenter();
 		TH1F *h=voxelVector[i]->GetClassHist();
 		int binmax = h->GetMaximumBin();
 		//double x = h->GetXaxis()->GetBinCenter(binmax);
 		std::cout << "Num of PoCA points for index : "<< i << " : Voxel Number : " << voxelVector[i]->GetVoxelNum()
 				  << " : " << voxelVector[i]->GetPocaPointsVector().size()
 				  << " : Majority Class : " << binmax << std::endl;
+		outVoxelFile << voxCenter.x() << " " << voxCenter.y() << " " << voxCenter.z()  
+					 << " " << binmax << " " << binmax << std::endl;
+
 	}
 	voxelVector[0]->FillClassHist();
 	voxelVector[0]->GetClassHist()->Draw();
 
-
+	outVoxelFile.close();
 	fApp->Run();
 
 	return 0;
