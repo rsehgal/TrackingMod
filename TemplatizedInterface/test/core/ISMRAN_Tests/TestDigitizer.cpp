0ab45d0cd6e3fa3b81ea31e2dd81cb86d37f534c
Raman Sehgal <raman.sehgal@cern.ch>
2020-08-11 17:54:51 +0530

    First very raw attempt to predict the hit position on the top surface of scintillator bar. NOT YET verified by any means.
    Currently it is NOT using any positional calibration parameters.
    
    Also added ** layerIndex ** in ** ScintillatorBar ** struct.

--- TemplatizedInterface/test/core/ISMRAN_Tests/TestDigitizer.cpp
+++ TemplatizedInterface/test/core/ISMRAN_Tests/TestDigitizer.cpp
@@ -41,6 +41,26 @@ unsigned int numOfBarsInEachLayer=9;
 std::map<std::string,TH1D*> barsEnergyMap;
 std::vector<TH1D*> vecOfEnergyHist;
 
+double fwhm = 2.355 * 5.66353 ;
+
+#define cm 10
+#define mm 10
+
+struct Point3D{
+	double x;
+	double y;
+	double z;
+
+	Point3D(double s_x, double s_y, double s_z){
+		x = s_x;
+		y = s_y;
+		z = s_z;
+	}
+
+	Point3D(){}
+
+};
+
 struct ScintillatorBar{
 	std::string  scintName;    //! board #  and channel number ( its packed in as follows ) //! board*16 + chno.
 
@@ -57,6 +77,9 @@ struct ScintillatorBar{
 	UInt_t    time;    //! real computer time in sec
 
 	unsigned short barIndex;
+	unsigned short layerIndex;
+
+	Point3D hitPosition;
 
 
 	ScintillatorBar(unsigned short l_channelNear, unsigned short l_channelFar,
@@ -64,6 +87,7 @@ struct ScintillatorBar{
 					UInt_t l_qlongNear,	UInt_t l_qlongFar, UInt_t l_time){
 
 		barIndex = l_channelNear/2;
+		layerIndex = barIndex/numOfBarsInEachLayer;
 		scintName="PsBar"+std::to_string(barIndex)+"-"+std::to_string(l_channelNear)+"-"+std::to_string(l_channelFar);
 		qlongNear=l_qlongNear;
 		qlongFar=l_qlongFar;
@@ -73,6 +97,8 @@ struct ScintillatorBar{
 		tsmallTimeStamp = (tstampNear < tstampFar) ? tstampNear : tstampFar;
 		deltaTstamp=tstampNear-tstampFar;
 		time=l_time;
+		EstimateHitPosition();
+
 	}
 
 	ScintillatorBar(){}
@@ -80,9 +106,53 @@ struct ScintillatorBar{
 	void Print(){
 
 		std::cout << scintName << " , " << qlongNear << " , " << qlongFar << " , " << qlongMean << " , "
-				  << tstampNear  << " , " << tstampFar << " , " << tsmallTimeStamp << " , " << deltaTstamp << " , " << time << std::endl;
+				  << tstampNear  << " , " << tstampFar << " , " << tsmallTimeStamp << " , " << deltaTstamp << " , "
+				  << time << " , " << " ( " << hitPosition.x << " , " << hitPosition.y << " , " << hitPosition.z << " ) " << std::endl;
 
 	}
+
+	/*
+	 * Calculates the estimated hit position in cm along Y,
+	 * deltaTstamp should be in ns
+	 */
+	void EstimateHitPositionAlongY(){
+
+		hitPosition.y = 30*(deltaTstamp/1000.0)/fwhm;
+		if(hitPosition.y < 0){
+			hitPosition.y = -1.*hitPosition.y;
+		}
+	}
+
+	/*
+	 * Calculates the estimated hit position in cm along X,
+	 *
+	 * For the time being, this can be mid point along X.
+	 * can be 5cm
+	 */
+	void EstimateHitPositionAlongX(){
+		hitPosition.x = 5;
+	}
+
+	/*
+	 * Calculates the estimated hit position in cm along Z,
+	 *
+	 * For the time being, this can be level of top surface along Z.
+	 * i.e. multiple of 10,
+	 * Consider the bottom most layer is at 0, then its Z = 1*10cm
+	 * General logic
+	 * Z=(layerIndex+1)*10
+	 */
+	void EstimateHitPositionAlongZ(){
+		hitPosition.z = (layerIndex+1)*10.;
+	}
+
+	void EstimateHitPosition(){
+		EstimateHitPositionAlongX();
+		EstimateHitPositionAlongY();
+		EstimateHitPositionAlongZ();
+	}
+
+
 
 
 };
@@ -410,6 +480,7 @@ void FillCoincidenceHist(std::vector< std::vector<ScintillatorBar*> > muonTrackV
 	return;
 
 }
+
 
 //void DigitizerDiff();
 /*void DigitizerAll();
