fe230e8793cffd5d3e5c13de39553b532ebc5ce9
Raman Sehgal <raman.sehgal@cern.ch>
2020-08-14 16:01:38 +0530

    [TomoIsmran]; Code refactoring done, and tried to include PsBar.h in simulations

---
+++ ismran/inc/PsBar.cpp
@@ -0,0 +1,21 @@
+/*
+ * PsBar.cpp
+ *
+ *  Created on: 14-Aug-2020
+ *      Author: rsehgal
+ */
+
+#include "PsBar.h"
+
+namespace Tomography {
+
+PsBar::PsBar() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PsBar::~PsBar() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Tomography */
