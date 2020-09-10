cf5ab071b89c3c0e75fb012da350279ea98a6eba
Raman Sehgal <raman.sehgal@cern.ch>
2020-09-10 17:37:30 +0530

    [TomoIsmran]; Added Two new classes               (1) Nomenclature :  That basically helps in generating the file name for calibration data               (2) ReadCalibrationDataFile : This class enable to read raw calibration data files and store them                                             as function in ROOT, from where it can be read directly during                                             analysis.                                             Test with missing raw calibration data files from PS-71 to PS-90
                                                To get the complete calibration file, this needs to be merged with
                                                calibration file provided by Pawan.
    
                                                After that all the 81 Bars can be used for tracking.

---
+++ ismran/analysis/readCalibDataFile.cpp
@@ -0,0 +1,22 @@
+/*
+ * readCalibDataFile.cpp
+ *
+ *  Created on: 09-Sep-2020
+ *      Author: rsehgal
+ */
+
+#include "ReadCalibrationDataFile.h"
+#include <vector>
+#include <TApplication.h>
+
+int main(){
+	//TApplication *fApp = new TApplication("Test", NULL, NULL);
+	ReadCalibrationDataFile cb;
+	//cb.ReadDataFile();
+
+	//fApp->Run();
+
+	return 0;
+}
+
+
