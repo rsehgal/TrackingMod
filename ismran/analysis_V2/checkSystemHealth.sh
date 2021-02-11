f4fb85409a625bb0b1c25308981ce12000863f35
Raman Sehgal <sc.ramansehgal@gmail.com>
2021-02-11 18:01:47 +0530

    Added some more health checkup code, and some robotic scripts ** ../checkSystemHealth.sh ** :  This script will run the ** TestSystemHealth.cpp ** for all the data files, and write the corresponding pdf file and root files ** SystemHealthComparison.cpp ** : This code will read the ROOT SytemHealth Root files created above and write a pdf file with overlappign plots for different data sets. Really useful to compare the health at different dates.

---
+++ ismran/analysis_V2/checkSystemHealth.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+dir=$1
+mat=$2
+dt=$3
+haddcommand="hadd SystemHealth-$mat-$dt.root"
+inputfilenames=`ls $dir/*$dt*.root`
+rmcommand="rm -rf "
+for eachfile in $inputfilenames
+do
+	echo "Processing file : $eachfile ....."
+	justFileName=$(basename $eachfile .root)
+	outputFileName="SystemHealth_$justFileName.root "
+	haddcommand+=" $outputFileName"
+	rmcommand+=$outputFileName
+	./TestSystemHealth $eachfile
+done
+$haddcommand
+$rmcommand
---
+++ ismran/analysis_V2/test/SystemHealthComparison.cpp
@@ -0,0 +1,114 @@
+/*
+ * SystemHealthComparison.cpp
+ *
+ *  Created on: 11-Feb-2021
+ *      Author: rsehgal
+ */
+
+#include <iostream>
+#include <vector>
+#include "PairFinder.h"
+#include "Analyzer.h"
+#include "Calibration.h"
+#include <TApplication.h>
+#include <TCanvas.h>
+#include "Plotter.h"
+#include "includes.hh"
+#include "HardwareNomenclature.h"
+#include <TSpectrum.h>
+#include  "Histograms.h"
+
+int main(int argc, char *argv[]){
+
+	std::vector<std::string> vecOfDates = {"14Jan - Without Scatterer","18Jan - Aluminium","22Jan - Iron"};
+	std::vector<std::string> vecOfFileNames={"SystemHealth-WithoutScatterer-14Jan.root", "SystemHealth-Al-18Jan.root",
+											 "SystemHealth-Fe-22Jan.root"};//,"SystemHealth-Fe-03Feb.root"};
+
+	std::vector<TFile*> vecOfFilePointers;
+
+	for(unsigned int i = 0 ; i < vecOfFileNames.size() ; i++){
+		vecOfFilePointers.push_back(new TFile(vecOfFileNames[i].c_str(),"READ"));
+	}
+	std::string rmfiles="";
+	for(unsigned int i = 0 ; i < vecOfBarsNamess.size() ; i++){
+		TCanvas *can = new TCanvas(vecOfBarsNamess[i].c_str(),vecOfBarsNamess[i].c_str());
+		can->Divide(2,3);
+
+		{TLegend *leg = new TLegend();
+		(can->cd(1))->SetLogy();
+		for(unsigned int j = 0 ; j < vecOfFileNames.size() ; j++){
+			std::string histName = vecOfBarsNamess[i]+"-Near";
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->SetLineColor(j+1);
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->Draw("same");
+			leg->AddEntry(((TH1D*)vecOfFilePointers[j]->Get(histName.c_str())),vecOfDates[j].c_str(),"l");
+			leg->Draw("same");
+		}
+		}
+
+		{TLegend *leg = new TLegend();
+		(can->cd(2))->SetLogy();
+		for(unsigned int j = 0 ; j < vecOfFileNames.size() ; j++){
+			std::string histName = vecOfBarsNamess[i]+"-Far";
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->SetLineColor(j+1);
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->Draw("same");
+			leg->AddEntry(((TH1D*)vecOfFilePointers[j]->Get(histName.c_str())),vecOfDates[j].c_str(),"l");
+			leg->Draw("same");
+		}
+		}
+
+		{TLegend *leg = new TLegend();
+		(can->cd(3))->SetLogy();
+		for(unsigned int j = 0 ; j < vecOfFileNames.size() ; j++){
+			std::string histName = vecOfBarsNamess[i]+"-QMean";
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->SetLineColor(j+1);
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->Draw("same");
+			leg->AddEntry(((TH1D*)vecOfFilePointers[j]->Get(histName.c_str())),vecOfDates[j].c_str(),"l");
+			leg->Draw("same");
+		}
+		}
+
+		{TLegend *leg = new TLegend();
+		(can->cd(4))->SetLogy();
+		for(unsigned int j = 0 ; j < vecOfFileNames.size() ; j++){
+			std::string histName = vecOfBarsNamess[i]+"-QMeanCorrected";
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->SetLineColor(j+1);
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->Draw("same");
+			leg->AddEntry(((TH1D*)vecOfFilePointers[j]->Get(histName.c_str())),vecOfDates[j].c_str(),"l");
+			leg->Draw("same");
+		}
+		}
+
+		{TLegend *leg = new TLegend();
+		(can->cd(5))->SetLogy();
+		for(unsigned int j = 0 ; j < vecOfFileNames.size() ; j++){
+			std::string histName = vecOfBarsNamess[i]+"-DelT";
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->SetLineColor(j+1);
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->Draw("same");
+			leg->AddEntry(((TH1D*)vecOfFilePointers[j]->Get(histName.c_str())),vecOfDates[j].c_str(),"l");
+			leg->Draw("same");
+		}
+		}
+
+		{TLegend *leg = new TLegend();
+		(can->cd(6))->SetLogy();
+		for(unsigned int j = 0 ; j < vecOfFileNames.size() ; j++){
+			std::string histName = vecOfBarsNamess[i]+"-DelTCorrected";
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->SetLineColor(j+1);
+			((TH1D*)vecOfFilePointers[j]->Get(histName.c_str()))->Draw("same");
+			leg->AddEntry(((TH1D*)vecOfFilePointers[j]->Get(histName.c_str())),vecOfDates[j].c_str(),"l");
+			leg->Draw("same");
+		}
+		}
+
+		std::string canFileName = vecOfBarsNamess[i]+".pdf";
+		rmfiles += (canFileName+" ");
+		can->Print(canFileName.c_str());
+
+		delete can;
+	}
+
+	std::string rmcommand="rm -rf "+rmfiles;
+	std::string mergecommand="pdfunite "+rmfiles+"SystemHealth-Comparison.pdf";
+	system(mergecommand.c_str());
+	system(rmcommand.c_str());
+}
