a16ca056b41cfbd3ccd52c2b05a215a587d01d29
Raman Sehgal <sc.ramansehgal@gmail.com>
2021-05-23 08:30:09 +0530

    Applied clang-format to main.cpp of Simulation_V3

--- ismran/simulations_V3/main.cpp
+++ ismran/simulations_V3/main.cpp
@@ -1,11 +1,11 @@
+#include "B1ActionInitialization.hh"
+#include "FTFP_BERT.hh"
 #include "G4RunManager.hh"
 #include "G4VModularPhysicsList.hh"
 #include "G4VUserPhysicsList.hh"
-#include "MyPrimaryGeneratorAction.h"
 #include "MyDetectorConstruction.h"
+#include "MyPrimaryGeneratorAction.h"
 #include "QBBC.hh"
-#include "FTFP_BERT.hh"
-#include "B1ActionInitialization.hh"
 
 #ifdef G4VIS_USE
 #include "G4VisExecutive.hh"
@@ -15,97 +15,93 @@
 #include "G4UIExecutive.hh"
 #endif
 
-#include "G4UImanager.hh"
 #include "FTFP_BERT.hh"
+#include "G4UImanager.hh"
 
-#include "includes.hh"
 #include "Analyzer.h"
 #include "HardwareNomenclature.h"
+#include "includes.hh"
 ///#include "PsBar.h"
 
 //#include "PhysicsList.h"
 //#include "HodoScope.h"
 //#include "QGSP_BIC_EMY.hh"
 /*
+#include "G4UIExecutive.hh"
 #include "G4UImanager.hh"
 #include "G4VisExecutive.hh"
-#include "G4UIExecutive.hh"
 */
 
 //#include "GeantVisualizer.h"
 #include "Calibration.h"
 
-
 #include "Randomize.hh"
 
+int main(int argc, char *argv[]) {
 
-int main(int argc, char *argv[]){
+  GenerateScintMatrixXYCenters();
+  lite_interface::IsSimulation = true;
 
-	GenerateScintMatrixXYCenters();
-	lite_interface::IsSimulation = true;
-
-	//lite_interface::Calibration *calib = lite_interface::Calibration::instance("/home/rsehgal/BackBoneSoftwares/ismranData/completeCalib.root");
-	lite_interface::Calibration *calib = lite_interface::Calibration::instance("/home/rsehgal/BackBoneSoftwares/ismranData/completeCalib2.root");
+  // lite_interface::Calibration *calib =
+  // lite_interface::Calibration::instance("/home/rsehgal/BackBoneSoftwares/ismranData/completeCalib.root");
+  lite_interface::Calibration *calib = lite_interface::Calibration::instance("completeCalib2.root");
 
   G4RunManager *runManager = new G4RunManager;
-  //G4VUserPhysicsList* phyList = new MyPhysics;
+  // G4VUserPhysicsList* phyList = new MyPhysics;
 
-  //Detector Construction
+  // Detector Construction
   runManager->SetUserInitialization(new MyDetectorConstruction());
-  //runManager->SetUserInitialization(new HodoScope());
+  // runManager->SetUserInitialization(new HodoScope());
 
-  //Physics List
-  //For user define PhysicsList uncomment the line below
-  //runManager->SetUserInitialization(new MyPhysics);
-  
-  //Using Pre defined PhysicsList
+  // Physics List
+  // For user define PhysicsList uncomment the line below
+  // runManager->SetUserInitialization(new MyPhysics);
+
+  // Using Pre defined PhysicsList
   runManager->SetUserInitialization(new FTFP_BERT);
 
-  //Primary Generator Action
-  //runManager->SetUserAction(new MyPrimaryGeneratorAction());
+  // Primary Generator Action
+  // runManager->SetUserAction(new MyPrimaryGeneratorAction());
 
   // User action initialization
   runManager->SetUserInitialization(new B1ActionInitialization());
 #ifdef G4VIS_USE
-    // Visualization manager construction
-    G4VisManager* visManager = new G4VisExecutive;
-    // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
-    // G4VisManager* visManager = new G4VisExecutive("Quiet");
-    visManager->Initialize();
+  // Visualization manager construction
+  G4VisManager *visManager = new G4VisExecutive;
+  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+  // G4VisManager* visManager = new G4VisExecutive("Quiet");
+  visManager->Initialize();
 #endif
 
-    // Get the pointer to the User Interface manager
-    G4UImanager* UImanager = G4UImanager::GetUIpointer();
+  // Get the pointer to the User Interface manager
+  G4UImanager *UImanager = G4UImanager::GetUIpointer();
 
-    if (argc>1) {
-        // execute an argument macro file if exist
-        G4String command = "/control/execute ";
-        G4String fileName = argv[1];
-        UImanager->ApplyCommand(command+fileName);
-    }
-    else {
-        //Needed with Qt sessions
-        // Initialize Geant4 kernel
-        runManager->Initialize();
+  if (argc > 1) {
+    // execute an argument macro file if exist
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command + fileName);
+  } else {
+    // Needed with Qt sessions
+    // Initialize Geant4 kernel
+    runManager->Initialize();
 
-        // start interactive session
+    // start interactive session
 #ifdef G4UI_USE
-        G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+    G4UIExecutive *ui = new G4UIExecutive(argc, argv);
 #ifdef G4VIS_USE
-        UImanager->ApplyCommand("/control/execute init_vis.mac");
+    UImanager->ApplyCommand("/control/execute init_vis.mac");
 #else
-        UImanager->ApplyCommand("/control/execute init.mac");
+    UImanager->ApplyCommand("/control/execute init.mac");
 #endif
-        if (ui->IsGUI())
-            UImanager->ApplyCommand("/control/execute gui.mac");
-        ui->SessionStart();
-        delete ui;
+    if (ui->IsGUI())
+      UImanager->ApplyCommand("/control/execute gui.mac");
+    ui->SessionStart();
+    delete ui;
 #endif
-    }
-
+  }
 
   delete runManager;
 
   return 0;
-
 }
