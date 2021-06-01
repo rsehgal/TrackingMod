820fa838eec8ce3b6db3685d9ce55117ec1ba906
Raman Sehgal <sc.ramansehgal@gmail.com>
2021-06-01 07:52:26 +0530

    Also tried with Train test split, and after splitting training portion of data is used to train the model, and then the model is tested over test portion of data, and it give good r_sq score for the exact hit data

--- ismran/ml/regModel.py
+++ ismran/ml/regModel.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 df = pd.read_csv('_simulated_10L_training.txt',names=['layer9_z','layer8_x','layer7_z','layer8_z'])
 #print(df.head(10))
@@ -7,8 +8,17 @@ df = pd.read_csv('_simulated_10L_training.txt',names=['layer9_z','layer8_x','lay
 x=df[['layer9_z','layer8_x','layer7_z']]
 y=df['layer8_z']
 print(x.head(10))
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.85)
+
 #x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
 #y = np.array([5, 20, 14, 32, 22, 38])
-model = LinearRegression().fit(x, y)
-r_sq = model.score(x, y)
+
+#model = LinearRegression().fit(x, y)
+#r_sq = model.score(x, y)
+#print('coefficient of determination:', r_sq)
+
+model = LinearRegression()
+model.fit(x_train,y_train)
+r_sq=model.score(x_test,y_test)
 print('coefficient of determination:', r_sq)
